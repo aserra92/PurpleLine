90fd8cbf016864aca96e07b3ebf6d1593ecd2640
aserra92 <aria.serrafelip@gmail.com>
2018-06-30 14:48:40 +0200

    Now the Glew and glfw are working and showing a triangle

--- PurpleLine/src/main.cpp
+++ PurpleLine/src/main.cpp
@@ -1,8 +1,78 @@
 #include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-	
+	if (!glfwInit())
+	{
+		std::cout << "Library GLFW cannot be initialized" << std::endl; //TODO: do with log
+		return false;
+	}
+
+	GLFWwindow *window;
+	window = glfwCreateWindow(1280, 720, "PurpleLine", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		std::cout << "GLFW Window cannot be created" << std::endl; //TODO: do with log
+		return false;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW failed to initialize" << std::endl; //TODO: do with log
+		return false;
+	}
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl; //TODO: do with log
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+	};
+	// This will identify our vertex buffer
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	system("PAUSE");
 	return 0;
 }
